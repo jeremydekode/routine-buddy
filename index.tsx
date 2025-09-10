b694dc3fbbcb6a8c16648d9a6a07eb36381bfce7
jeremydekode <jeremy@dekode.ai>
2025-09-10 21:21:24 +0800

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,8 +1,8 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AppProvider } from './hooks/useAppContext';
+import { AuthGate } from './components/AuthGate';   // 👈 add this
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
@@ -12,8 +12,10 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <AppProvider>
-      <App />
-    </AppProvider>
+    <AuthGate>
+      <AppProvider>
+        <App />
+      </AppProvider>
+    </AuthGate>
   </React.StrictMode>
 );
